996e9d9dc5b86fedd9a7d279f2ac9641564181c7
HiNT <2001hieunt89@gmail.com>
2022-09-13 07:15:44 +0700

    slider product

--- assets/js/product.js
+++ assets/js/product.js
@@ -78,3 +78,96 @@ const evaluates = [
         point : 5,
     }
 ]
+const $ = document.querySelector.bind(document)
+const $$ = document.querySelectorAll.bind(document)
+const imgProduct = $('#img-visi')
+const imgProductHidden = $('#img-hidden')
+const btnSearch = $('#btn-search-hidden')
+const listItem = $('.product__items')
+const sliderProduct = {
+    products : products,
+    currentStart : 0,
+    limit :5, 
+    render : function () {
+        let i = this.currentStart
+        let max = this.limit
+        let htmls = '';
+        for(i;i<max;i++) {
+            htmls += `<div href="#" class="product__item">
+            <div class="product__itemImg">
+                <img src="${this.products[i].productImg}" alt="" id="img-visi">
+                <img src="${this.products[i].productImgHover}" alt="" class="hidden" id="img-hidden">
+                <span class="product__sale">
+                    -11%
+                </span>
+                <button class="hidden" id="btn-search-hidden">
+                    <i class="fa-solid fa-magnifying-glass"></i>
+                </button>
+
+                <button class="product__btnAdds">
+                   <span class="product__btnAdd">
+                    <i class="fa-sharp fa-solid fa-cart-plus"></i>
+                   </span>
+                    <span class="product__btnAddText">thêm vào giỏ</span>
+                </button>
+            </div>
+            <div class="product__detail">
+                <!-- name -->
+                <h3 class="product__name">
+                    ${this.products[i].productName}
+                </h3>
+                <!-- star -->
+                <ul class="product__evaluates">
+                    <li class="product__evaluate">
+                        <i class="fa-solid fa-star"></i>
+                    </li>
+                    <li class="product__evaluate">
+                        <i class="fa-solid fa-star"></i>
+                    </li>
+                    <li class="product__evaluate">
+                        <i class="fa-solid fa-star"></i>
+                    </li>
+                    <li class="product__evaluate">
+                        <i class="fa-solid fa-star"></i>
+                    </li>
+                    <li class="product__evaluate">
+                        <i class="fa-solid fa-star"></i>
+                    </li>
+                    <li class="product__evaluate">(0)</li>
+                </ul>
+                <!-- price -->
+                <div class="product__prices">
+                    <div class="product__price product__price--sale">
+                    ${this.products[i].productSale}
+                    </div>
+                    <div class="product__price product__price--cost">
+                    ${this.products[i].productCost}
+                    </div>
+                </div>
+            </div>
+        </div>`
+        }
+        listItem.innerHTML = htmls
+    },
+    handleProduct : function () {
+        //hover img product
+        imgProduct.onmouseover = function () {
+            imgProductHidden.classList.remove('hidden')
+            imgProduct.classList.add('hidden')
+            btnSearch.classList.remove('hidden')
+        }
+        imgProductHidden.onmouseout = function () {
+            imgProductHidden.classList.add('hidden')
+            imgProduct.classList.remove('hidden')
+            btnSearch.classList.add('hidden')
+        }
+    },
+    start : function ()  {
+        // render list product
+        this.render()
+        // lang ngh va xu ly su kien
+        this.handleProduct()
+
+    }
+}
+sliderProduct.start()
